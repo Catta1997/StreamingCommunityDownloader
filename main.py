e95eef3b07ffa43967df9fddb5f664c8f4f447e1
Catta1997 <edoardo.cattarin@gmail.com>
2020-07-28 23:06:38 +0200

    Initial support for youtube_dl

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import sys
 import getopt
 import html
 import json
+from shutil import which
+from youtube_dl import YoutubeDL
+from tqdm import trange
+from tqdm import tqdm
+from time import sleep
+import sys
+import re
 from bs4 import BeautifulSoup
 #config
 config = {'crawl_path': None, 'download_path': None}
 one_file = True #create a single crawljob for Series with multiple season
 
+Down_YT = False # BETA
 def interactive_mode():
     try:
         keyword = input("Keyword: ")
@@ -127,14 +135,17 @@ def main():
     episodes_2 = json_data_2
     ep_json_2 = json.loads(episodes_2)
     text = ep_json_2[0]['episodes']
-    if not one_file:
-        x = 1
-        for k in ep_json_2:
-            test = k['episodes']
-            create_crawl(slug,id,test,x)
-            x +=1
+    if Down_YT:
+        youtube_downloader(ep_json_2,slug,id)
     else:
-        create_crawl_all(slug,id,ep_json_2)
+        if not one_file:
+            x = 1
+            for k in ep_json_2:
+                test = k['episodes']
+                create_crawl(slug,id,test,x)
+                x +=1
+        else:
+            create_crawl_all(slug,id,ep_json_2)
 
 def help():
     usage = f"\t-k, --keyword (str):\t\tSpecify the keyword to search\n" \
@@ -142,6 +153,62 @@ def help():
             f"\t--crawlpath (Path):\t\tDestination folder for the crawljobs\n" \
             f"\t-h, --help:\t\t\tShow this screen\n"
     print(usage)
+
+
+
+def youtube_downloader(ep_list,slug,id):
+    content_dir = os.path.join("Download", slug)
+    if not os.path.exists(content_dir):
+        os.makedirs(content_dir)
+    ffmpeg_local = ""
+    if which("ffmpeg") is None:
+        _dir = os.path.dirname(os.path.realpath(__file__))
+        ffmpeg_dir_files = os.listdir(os.path.join(_dir, "ffmpeg"))
+        ffmpeg_dir_files.remove("readme.md")
+        # If the directory is ambiguous stop the script
+        if len(ffmpeg_dir_files) > 1:
+            print("Controlla che la cartella ffmpeg contwnga solo il readme e la cartella con òa build di ffmpeg")
+            quit()
+        elif len(ffmpeg_dir_files) == 0:
+            print("Installa ffmpeg, consulta la pagina su GitHub per maggiori informazioni")
+            quit()
+        ffmpeg_local = os.path.join( _dir, "ffmpeg", ffmpeg_dir_files[0], "bin")
+    pbar = tqdm(ep_list, bar_format=("{l_bar}{bar}| {n_fmt}/{total_fmt}"))
+    for episode in pbar:
+        ydl_opts = {
+            "format": "best",
+            "outtmpl": "%s/%s.%%(ext)s" % (content_dir, slug),
+            "continuedl": True,
+            "quiet" : True,
+            "simulate":True, # Debug: simulate a dowload 
+        }
+        if ffmpeg_local:
+            ydl_opts["ffmpeg_location"] = ffmpeg_local
+        ep_list_2 = episode['episodes']
+        season = 1
+        episode_n = 1
+        for i in ep_list_2:
+            ep_id = i['id']
+            with YoutubeDL(ydl_opts) as ydl:
+            #meta = ydl.extract_info(episode.link, download=False) 
+                pbar.set_description("Processing: %s" % slug + ' Season ' + str(season) + ' ep ' + str(episode_n))
+                link = "https://streamingcommunity.to/watch/%s?e=%s\n"%(id,ep_id)
+                r = requests.get(url = link, params = {}) 
+                pastebin_url = r.text 
+                my_html = pastebin_url
+                parsed_html = BeautifulSoup(my_html,"html.parser")
+                json_data = parsed_html.find('video-player')['response']
+                episodes = json_data
+                ep_json = json.loads(episodes)
+                try:
+                    link = ep_json['video_url']
+                    ydl.download([link])
+                except KeyboardInterrupt:
+                    sys.exit()
+                episode_n +=1
+            #f.write("\ttext=https://streamingcommunity.to/watch/%s?e=%s\n"%(id,ep_id))
+        season += 1
+        episode_n = 1
 
 if __name__ == "__main__":
     main()
